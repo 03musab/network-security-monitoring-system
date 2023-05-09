82b6b9bca13f4cd52e5e47a789ce0d140329cf94
03musab <115150570+03musab@users.noreply.github.com>
2023-05-09 22:50:33 +0530

    Update Sidebar.jsx

--- src/component/Dashboard/Sidebar.jsx
+++ src/component/Dashboard/Sidebar.jsx
@@ -147,7 +147,7 @@ const Sidebar = () => {
               color={colors.grey[300]}
               sx={{ m: "15px 0 5px 20px" }}
             >
-              Activities
+              Activity
             </Typography>
             <Item
               title="Contact"
