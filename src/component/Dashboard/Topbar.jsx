17578fd131cf53f031d4f1180481ec318a264ecf
03musab <03.musa.1@gmail.com>
2023-05-09 15:24:51 +0530

    project completed

---
+++ src/component/Dashboard/Topbar.jsx
@@ -0,0 +1,86 @@
+import React, { useContext } from 'react'
+import { Box, IconButton, useTheme, Select, MenuItem, Typography } from '@mui/material';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material/InputBase';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+// import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import fire from '../../config/Fire';
+import { AuthContext } from '../../context/Auth';
+
+
+const Topbar = () => {
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  const { currentUser } = useContext(AuthContext);
+
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        {/* <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton> */}
+        
+        <Select
+          sx={{
+            marginTop: 0,
+            width: 90,
+            height: 40,
+          }}
+        >
+          <MenuItem onClick={() => fire.auth().signOut()} >
+            <IconButton>
+              <PersonOutlinedIcon />
+            </IconButton>
+            <Typography variant="h5"
+              color={colors.grey[100]}
+              fontWeight="bold"
+              sx={{ m: "0 0 0 0" }}>
+              LogOut:  {currentUser.email}
+            </Typography>
+          </MenuItem>
+          <MenuItem onClick={colorMode.toggleColorMode}>
+            <IconButton >
+              {theme.palette.mode === "dark" ? (
+                <DarkModeOutlinedIcon />
+              ) : (
+                <LightModeOutlinedIcon />
+              )}
+            </IconButton>
+            <Typography variant="h5"
+              color={colors.grey[100]}
+              fontWeight="bold"
+              sx={{ m: "0 0 0 0" }}>
+              ColorMode
+            </Typography>
+          </MenuItem>
+        </Select>
+        {/* <IconButton>
+        <SettingsOutlinedIcon/>
+      </IconButton> */}
+
+      </Box>
+    </Box>
+  )
+}
+
+export default Topbar;  
