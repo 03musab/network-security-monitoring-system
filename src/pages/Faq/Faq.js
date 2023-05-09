9cdd273bece5d3e4da9eebdaa143426b0ff72de0
03musab <115150570+03musab@users.noreply.github.com>
2023-05-09 21:46:45 +0530

    Update Faq.js
    updated faqs

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -22,66 +22,43 @@ const FAQ = () => {
 
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            An Important Question
-          </Typography>
+          <Typography color={colors.greenAccent[500]} variant="h5">What is Network Security Monitoring System</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Our Network Monitoring System allows users to keep track of their network's activity and ensure its security. It provides features such as  session time monitoring, and user management even key logging. These features enable individuals and businesses to have better control over their networks, limit the access of unwanted sites or users, and prevent unauthorized access to sensitive data.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            Another Important Question
-          </Typography>
+          <Typography color={colors.greenAccent[500]} variant="h5">Is it crucial for buisness?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Network monitoring is very crucial for any business. Today, networks span globally, having multiple links established between geographically separated data centres, public and private clouds. This creates multifield challenges in network management. Network admins need to be more proactive and agile in monitoring network performance.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            Your Favorite Question
-          </Typography>
+          <Typography color={colors.greenAccent[500]} variant="h5">Conclusion</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          In conclusion, a security monitoring system is an essential tool for protecting your organization's assets, data, and people. With its advanced technology and real-time monitoring capabilities, it can detect and prevent security threats before they cause any harm. By implementing a comprehensive security monitoring system, you can significantly enhance your organization's security posture and reduce the risk of security breaches.
+Overall, a security monitoring system is a wise investment for any organization that values its security and wants to safeguard against potential threats.
+
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            Some Random Question
-          </Typography>
+          <Typography color={colors.greenAccent[500]} variant="h5">Some handpicked best features in this Project.</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography color={colors.greenAccent[500]} variant="h5">
-            The Final Question
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          User management, has his own workspace and even key logging. 
           </Typography>
         </AccordionDetails>
       </Accordion>
